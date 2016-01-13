2987725588157118f8b6a2b89adfdee119daf93a
A. Yakovlev <nhm7@yandex.ru>
2016-01-13 03:38:01 +0300

    ver 0.1 _phpinfo.php

--- _phpinfo.php
+++ _phpinfo.php
@@ -1,3 +1,4 @@
 <?php
-echo 'Мое имя пользователя: ' .$_ENV["OPENSHIFT_MYSQL_DB_USERNAME"] . '!';
+#echo 'Мое имя пользователя: ' .$_ENV["OPENSHIFT_MYSQL_DB_USERNAME"] . '!';
+<input type="text" value="{$DB_HOSTNAME}" name="db_hostname">
 ?>
