a74fda2242c1a43172b865993133a8bf532f43c5
A. Yakovlev <nhm7@yandex.ru>
2016-01-13 04:14:51 +0300

    ver 0.8 modules/Install/views/Index.php Step4

--- modules/Install/views/Index.php
+++ modules/Install/views/Index.php
@@ -102,7 +102,7 @@ class Install_Index_view extends Vtiger_View_Controller {
 		$viewer->assign('DB_HOSTNAME', $_ENV['OPENSHIFT_MYSQL_DB_HOST']);
 		$viewer->assign('DB_USERNAME', $_ENV['OPENSHIFT_MYSQL_DB_USERNAME']);
 		$viewer->assign('DB_PASSWORD', $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']);			
-		$viewer->assign('DB_NAME', $_ENV['OPENSHIFT_GEAR_NAME']);
+		$viewer->assign('DB_NAME', $_ENV['OPENSHIFT_APP_NAME']);
 		$viewer->assign('ADMIN_NAME', $defaultParameters['admin_name']);	
 		$viewer->assign('ADMIN_LASTNAME', $defaultParameters['admin_lastname']);	
 		$viewer->assign('ADMIN_PASSWORD', $defaultParameters['admin_password']);	
